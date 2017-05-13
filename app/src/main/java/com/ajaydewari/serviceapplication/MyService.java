4ae5e6507efa50ee57143008d199e67cb480789c
ajaydewari <ajdewari@gmail.com>
2017-05-13 21:32:41 +0530

    Initial commit

---
+++ app/src/main/java/com/ajaydewari/serviceapplication/MyService.java
@@ -0,0 +1,47 @@
+package com.ajaydewari.serviceapplication;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.IntDef;
+
+import java.util.Random;
+
+public class MyService extends Service {
+
+    int randomNumber;
+    private final static int MAX=100;
+    private final static int MIN=0;
+    private boolean isGenerater;
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, /*@IntDef(value = {Service.START_FLAG_REDELIVERY, Service.START_FLAG_RETRY}, flag = true) */int flags, int startId) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+        return START_STICKY;
+    }
+private void generateRandomNum(){
+    while(isGenerater){
+        randomNumber=new Random().nextInt(MIN)+MAX;
+    }
+
+}
+
+private int getRandomNumber(){
+
+        return randomNumber;
+    }
+}
