94cc16d70eb7f5997242bb2f80c6d92cb4de56ac
ajaydewari <ajdewari@gmail.com>
2017-05-14 21:58:44 +0530

    initial commit

--- app/src/main/java/com/ajaydewari/serviceapplication/MainActivity.java
+++ app/src/main/java/com/ajaydewari/serviceapplication/MainActivity.java
@@ -2,12 +2,16 @@ package com.ajaydewari.serviceapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnStart, btnStop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStart= (Button) findViewById(R.id.btnStart);
+        btnStop= (Button) findViewById(R.id.btnStop);
     }
 }
